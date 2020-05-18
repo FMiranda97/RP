20829d6f883204fcb09f1b0a0ea8698c6948519a
Miguel Arieiro <16050685+MiguelArieiro@users.noreply.github.com>
2020-05-18 22:11:44 +0100

    Added SVM OAO (WIP)
    TODO C tunning/testing

---
+++ app/appSVM.m
@@ -0,0 +1,52 @@
+function [svm_model, average_error, standard_dev] = appSVM (app, data, n_runs, training, testing, n_classes)
+    %% SVM parameter tuning
+    c_pot = [-25:10];
+    C=2.^c_pot;
+
+    %% Model evaluation
+
+    [models, error_rate] = SVM (data, n_classes, training, testing, n_runs);
+
+    if n_runs > 1
+        average_err = mean(error_rate);
+        standard_dev = std(error_rate);
+    else
+        average_err = error_rate;
+        standard_dev = zeros(1,numel(error_rate));
+    end
+
+%     app.UIAxes.cla;
+%     plot(c_pot, average_err, 'o');
+%     ylabel('Testing Error (%)');
+%     set(gca, 'xtick', c_pot);
+%     set(gca,'xticklabel', strcat('2^', cellfun(@num2str,num2cell(c_pot), 'UniformOutput',0)));
+%     hold on;
+%     errorbar(c_pot, average_err, standard_dev);
+%     axis([c_pot(1) c_pot(end) 0 100]);
+
+    %%Inspect for a svm_model Classifier
+    ix=find(average_err==min(average_err));
+    ix=ix(1);
+    fprintf("\nAverage svm_model C value = 2^%d\n", c_pot(ix));
+    ix_min_err = find(error_rate(:,ix) == min(error_rate(:,ix)));
+    if numel(ix_min_err)>1
+        sel_class=models(ix_min_err,ix);
+        nsvs=zeros(1,numel(ix_min_err));
+        for m=1:numel(ix_min_err)
+            nsvs(m) = sel_class{m}.nsv;
+        end
+        ix_min_nsv = find(nsvs==min(nsvs));
+        svm_model=sel_class{ix_min_nsv};
+    else
+        svm_model=models{ix_min_err,ix};
+    end
+
+    %[ypred, dfce] = svmclass(data_te.X, svm_model)
+    %[FPR, FNR] = roc(dfce, data_te.y);
+    %figure(); plot(FPR,1-FNR);
+    %figure;ppatterns(data_tr); psvm(svm_model, struct('background',1));
+
+end
+
+
+
---
+++ classifiers/SVM.m
@@ -0,0 +1,41 @@
+function [models, error_rate] = SVM (data, n_classes, training, testing, n_runs)
+    %% SVM parameter tuning
+    c_pot = [-5:2];
+    C=2.^c_pot;
+
+    %% Model evaluation
+    error_rate = zeros(n_runs, numel(C));
+    models = cell(n_runs, numel(C));
+
+    for n=1:n_runs
+        ix=randperm(data.num_data);
+        ixtr=ix(1:floor(data.num_data*training));
+        ixte=ix(floor(data.num_data*testing)+1:end);
+
+        %training dataset
+        data_tr.X = data.X(:,ixtr);
+        data_tr.y = data.y(ixtr);
+        data_tr.dim = size(data_tr.X,1);
+        data_tr.num_data = size(data_tr.X,2);
+        data_tr.name = 'Traning set';
+        %testing dataset
+        data_te.X = data.X(:,ixte);
+        data_te.y = data.y(ixte);
+        data_te.dim = size(data_te.X,1);
+        data_te.num_data = size(data_te.X,2);
+        data_te.name = 'Testing set';
+
+        for co=1:numel(C)
+            fprintf("======\nrun=%d\nCost=%f\n======\n",n,C(co));
+            disp("Let's train");
+            model = oaosvm(data_tr, struct('solver','smo','ker', 'linear','C',C(co)));
+            ypred = svmclass(data_te.X, model);
+            error_rate(n, co) = cerror(ypred, data_te.y)*100;
+            models{n, co} = model;
+        end
+    end
+
+end
+
+
+
